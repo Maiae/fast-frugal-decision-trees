d978fea7edd12703cbab37079eefcfe331312a9e
Maia <ed.maia@anom.com.au>
2016-08-18 15:39:31 +1000

    add a model with training / testing set

--- titanic survival model.R
+++ titanic survival model.R
@@ -1,15 +1,28 @@
+# examples based on http://nathanieldphillips.com/2016/08/making-fast-good-decisions-with-the-fftrees-r-package/
 # load library
 library(FFTrees)
 
 # load Titanic data
 titanicData <- titanic
-titanicData$survived <- as.factor(ifelse(titanicData$survived == 1,
-                                         'yes', 'no'))
 
+# run fft
 titanic.fft <- fft(formula = survived ~ ., data = titanicData)
 
+# describe results
 titanic.fft
 
+# plot best tree
 plot(titanic.fft,
+     main = "Titanic Survivers FFT", 
+     decision.names = c("Died", "Survived"))
+
+# run trees with a 70% split for training and 30% for testing
+titanic.test.fft <- fft(formula = survived ~ ., data = titanicData, train.p = 0.70)
+
+# describe results
+titanic.test.fft
+
+# plot best tree
+plot(titanic.test.fft,
      main = "Titanic Survivers FFT", 
      decision.names = c("Died", "Survived"))
